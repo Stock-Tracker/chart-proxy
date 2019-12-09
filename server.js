8d87cd8a775e44c0e6ff01027a709334d5eb3961
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-12-09 14:43:33 -0500

    Fixes typo

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('client/dist'));
 app.use(express.static('client/public'));
-app.listen(() => { console.log(`Reverse Proxy is running on port ${PORT}`)});
+
+app.listen(PORT, () => { console.log(`Reverse Proxy is running on port ${PORT}`)});
