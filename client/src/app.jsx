e0f77843c01409bffb54fdd90097960ee34f54d7
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-12-09 15:41:58 -0500

    Adds basic static layout

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -10,8 +10,47 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        It's rendering!
+      <div className="react-root">
+        <div className="main-container">
+          <header>
+            <div id="logo">
+              logo goes here
+            </div>
+            <div id="search">
+              <input type="text"></input>
+            </div>
+            <nav>
+              <ul className="nav-list">
+                <li className="nav-list-item">Free Stock</li>
+                <li className="nav-list-item">Portfolio</li>
+                <li className="nav-list-item">Cash</li>
+                <li className="nav-list-item">Messages</li>
+                <li className="nav-list-item">Account</li>
+              </ul>
+            </nav>
+          </header>
+          <main>
+            <div className="row">
+              <div>
+                <div id="tags">
+                  <ul className="tag-list">
+                    <li className="tag-list-item">Computer Hardware</li>
+                    <li className="tag-list-item">100 Most Popular</li>
+                    <li className="tag-list-item">Computer Software</li>
+                  </ul>
+                </div>
+                <div id="chart"></div>
+                <div id="about"></div>
+                <div id="collections"></div>
+                <div id="news"></div>
+                <div id="analyst-ratings"></div>
+                <div id="earnings"></div>
+                <div id="people-also-bought"></div>
+              </div>
+              <aside></aside>
+            </div>
+          </main>
+        </div>
       </div>
     );
   }
